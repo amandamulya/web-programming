2c9d43077d7c2db93031492b9141000434be8cd4
focusme <amandamulya011@gmail.com>
2023-06-08 16:51:34 +0700

    Dev Web Focusme

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$no_wa = 628975845396;
+?>
+<!doctype html>
+<html>
+    <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Focus Me</title>
+    </head>
+    <body>
+        <a href="">Klik untuk wa</a>
+</body>
+</html>
